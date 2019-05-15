da29f5b6849acf9b0a5e27b0fbbd9d985b99836e
Nitesh Srivats <niteshsrivats.k@gmail.com>
2019-05-16 00:11:30 +0530

    Updated insert queries

---
+++ forms/Student/NewScholarshipTransaction.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Finance_Management.forms.Scholarship
+{
+    partial class NewScholarshipTransaction
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Submit = new System.Windows.Forms.Button();
+            this.LabelScholarshipName = new System.Windows.Forms.Label();
+            this.LabelUSN = new System.Windows.Forms.Label();
+            this.USNBox = new System.Windows.Forms.TextBox();
+            this.DataView = new System.Windows.Forms.DataGridView();
+            this.SearchBox = new System.Windows.Forms.TextBox();
+            this.LabelSearch = new System.Windows.Forms.Label();
+            this.ScholarshipNameList = new System.Windows.Forms.ListBox();
+            this.Back = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DataView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Submit
+            // 
+            this.Submit.BackColor = System.Drawing.SystemColors.Window;
+            this.Submit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Submit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Submit.Location = new System.Drawing.Point(410, 400);
+            this.Submit.Name = "Submit";
+            this.Submit.Size = new System.Drawing.Size(80, 30);
+            this.Submit.TabIndex = 4;
+            this.Submit.Text = "Submit";
+            this.Submit.UseVisualStyleBackColor = false;
+            this.Submit.Click += new System.EventHandler(this.Submit_Click);
+            // 
+            // LabelScholarshipName
+            // 
+            this.LabelScholarshipName.AutoSize = true;
+            this.LabelScholarshipName.Location = new System.Drawing.Point(24, 140);
+            this.LabelScholarshipName.Name = "LabelScholarshipName";
+            this.LabelScholarshipName.Size = new System.Drawing.Size(82, 34);
+            this.LabelScholarshipName.TabIndex = 2;
+            this.LabelScholarshipName.Text = "Scholarship\r\n     Name";
+            // 
+            // LabelUSN
+            // 
+            this.LabelUSN.AutoSize = true;
+            this.LabelUSN.Location = new System.Drawing.Point(24, 256);
+            this.LabelUSN.Name = "LabelUSN";
+            this.LabelUSN.Size = new System.Drawing.Size(37, 17);
+            this.LabelUSN.TabIndex = 3;
+            this.LabelUSN.Text = "USN";
+            this.LabelUSN.Click += new System.EventHandler(this.LabelUSN_Click);
+            // 
+            // USNBox
+            // 
+            this.USNBox.Location = new System.Drawing.Point(112, 253);
+            this.USNBox.MaxLength = 10;
+            this.USNBox.Name = "USNBox";
+            this.USNBox.Size = new System.Drawing.Size(100, 22);
+            this.USNBox.TabIndex = 1;
+            // 
+            // DataView
+            // 
+            this.DataView.AllowUserToOrderColumns = true;
+            this.DataView.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.DataView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataView.Location = new System.Drawing.Point(400, 60);
+            this.DataView.Name = "DataView";
+            this.DataView.RowTemplate.Height = 24;
+            this.DataView.Size = new System.Drawing.Size(480, 193);
+            this.DataView.TabIndex = 6;
+            // 
+            // SearchBox
+            // 
+            this.SearchBox.Location = new System.Drawing.Point(609, 315);
+            this.SearchBox.MaxLength = 255;
+            this.SearchBox.Name = "SearchBox";
+            this.SearchBox.Size = new System.Drawing.Size(100, 22);
+            this.SearchBox.TabIndex = 5;
+            this.SearchBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.SearchBox_KeyDown);
+            // 
+            // LabelSearch
+            // 
+            this.LabelSearch.AutoSize = true;
+            this.LabelSearch.Location = new System.Drawing.Point(550, 318);
+            this.LabelSearch.Name = "LabelSearch";
+            this.LabelSearch.Size = new System.Drawing.Size(53, 17);
+            this.LabelSearch.TabIndex = 7;
+            this.LabelSearch.Text = "Search";
+            this.LabelSearch.Click += new System.EventHandler(this.LabelSearch_Click);
+            // 
+            // ScholarshipNameList
+            // 
+            this.ScholarshipNameList.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.ScholarshipNameList.FormattingEnabled = true;
+            this.ScholarshipNameList.ItemHeight = 16;
+            this.ScholarshipNameList.Items.AddRange(new object[] {
+            "Bal Vidhya Scholarship",
+            "Excel Student Scholarship",
+            "Girl Child Scholarship",
+            "KCET Scholarship For Defence Wards",
+            "Krishi Vidhya Scholarship",
+            "Maritime Scholarship For Engineers",
+            "National College Scholarship",
+            "NCC Scholarship For Undergraduates",
+            "SC/ST Undergraduates Scholarship",
+            "Sri Visveswaraya Scholarship"});
+            this.ScholarshipNameList.Location = new System.Drawing.Point(112, 105);
+            this.ScholarshipNameList.Name = "ScholarshipNameList";
+            this.ScholarshipNameList.Size = new System.Drawing.Size(270, 100);
+            this.ScholarshipNameList.Sorted = true;
+            this.ScholarshipNameList.TabIndex = 3;
+            // 
+            // Back
+            // 
+            this.Back.BackColor = System.Drawing.SystemColors.Window;
+            this.Back.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Back.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Back.Location = new System.Drawing.Point(40, 30);
+            this.Back.Name = "Back";
+            this.Back.Size = new System.Drawing.Size(80, 30);
+            this.Back.TabIndex = 7;
+            this.Back.Text = "Back";
+            this.Back.UseVisualStyleBackColor = false;
+            this.Back.Click += new System.EventHandler(this.Back_Click);
+            // 
+            // NewScholarshipTransaction
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(882, 459);
+            this.Controls.Add(this.Back);
+            this.Controls.Add(this.ScholarshipNameList);
+            this.Controls.Add(this.LabelSearch);
+            this.Controls.Add(this.SearchBox);
+            this.Controls.Add(this.DataView);
+            this.Controls.Add(this.USNBox);
+            this.Controls.Add(this.LabelUSN);
+            this.Controls.Add(this.LabelScholarshipName);
+            this.Controls.Add(this.Submit);
+            this.Name = "NewScholarshipTransaction";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "New Scholarship Transaction";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ScholarshipTransaction_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.DataView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Submit;
+        private System.Windows.Forms.Label LabelScholarshipName;
+        private System.Windows.Forms.Label LabelUSN;
+        private System.Windows.Forms.TextBox USNBox;
+        private System.Windows.Forms.DataGridView DataView;
+        private System.Windows.Forms.TextBox SearchBox;
+        private System.Windows.Forms.Label LabelSearch;
+        private System.Windows.Forms.ListBox ScholarshipNameList;
+        private System.Windows.Forms.Button Back;
+    }
+}
---
+++ forms/Student/NewScholarshipTransaction.cs
@@ -0,0 +1,159 @@
+﻿using Finance_Management.forms.Navigation;
+using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Finance_Management.forms.Scholarship
+{
+    public partial class NewScholarshipTransaction : Form
+    {
+        private SQL_Operator sql_operator = new SQL_Operator();
+        private Validation validation = new Validation();
+        private NewStudentTransactionMenu studentTransactionMenu;
+        private string scholarshipID;
+        private string amount;
+
+        public NewScholarshipTransaction(NewStudentTransactionMenu studentTransactionMenu)
+        {
+            InitializeComponent();
+            this.studentTransactionMenu = studentTransactionMenu;
+        }
+
+        private string Get_Search_Query()
+        {
+            string sqlQuery = "SELECT USN, NAME \"Name\", GRADUATION_YEAR \"Graduation Year\" FROM STUDENT_INFO";
+            Regex searchExp = new Regex("[0-9]+");
+            if (searchExp.Match(SearchBox.Text).Success)
+            {
+                sqlQuery += " WHERE USN LIKE '%" + SearchBox.Text.ToUpper() + "%'";
+            }
+            else
+            {
+                sqlQuery += " WHERE NAME LIKE '%" + SearchBox.Text.ToUpper() + "%'";
+            }
+            return sqlQuery;
+        }
+
+        private void SearchBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyValue == 13)
+            {
+                string sqlQuery = Get_Search_Query();
+                sql_operator.Display(sqlQuery, DataView);
+            }
+        }
+
+        private bool Validate_Form()
+        {
+            if (!validation.USN(USNBox.Text.ToUpper()))
+            {
+                MessageBox.Show("Invalid USN", "Invalid USN Error");
+                return false;
+            }
+            else if (!validation.ListBox(ScholarshipNameList.Text))
+            {
+                MessageBox.Show("Select Scholarship Name", "Invalid Scholarship Name Error");
+                return false;
+            }
+            switch (ScholarshipNameList.Text)
+            {
+                case "KCET Scholarship For Defence Wards":
+                    scholarshipID = "KCET_DEF";
+                    amount = "-6500";
+                    break;
+                case "National College Scholarship ":
+                    scholarshipID = "NAT_COLL";
+                    amount = "-8000";
+                    break;
+                case "Bal Vidhya Scholarship":
+                    scholarshipID = "BAL_VID";
+                    amount = "-12500";
+                    break;
+                case "Girl Child Scholarship":
+                    scholarshipID = "GIRL_CHLD";
+                    amount = "-5000";
+                    break;
+                case "Sri Visveswaraya Scholarship":
+                    scholarshipID = "SRI_VIS";
+                    amount = "-7500";
+                    break;
+                case "NCC Scholarship For Undergraduates":
+                    scholarshipID = "NCC_UG";
+                    amount = "-15000";
+                    break;
+                case "Maritime Scholarship For Engineers":
+                    scholarshipID = "MAR_ENG";
+                    amount = "-9000";
+                    break;
+                case "Excel Student Scholarship":
+                    scholarshipID = "EXC_STU";
+                    amount = "-10000";
+                    break;
+                case "SC/ST Undergraduates Scholarship":
+                    scholarshipID = "SC_ST";
+                    amount = "-6000";
+                    break;
+                case "Krishi Vidhya Scholarship":
+                    scholarshipID = "KRI_VID";
+                    amount = "-3000";
+                    break;
+            }
+            return true;
+        }
+
+        private void Clear_All_Entries()
+        {
+            USNBox.Clear();
+            ScholarshipNameList.ClearSelected();
+            SearchBox.Clear();
+            DataView.DataSource = null;
+        }
+
+        private void Submit_Click(object sender, EventArgs e)
+        {
+            if (Validate_Form())
+            {
+                DateTime today = DateTime.Now;
+                string time = today.Year.ToString() + "-" + today.Month.ToString() + "-" + today.Day.ToString() + " " + today.Hour.ToString() + ":" + today.Minute.ToString() + ":" + today.Second.ToString();
+      
+                string sqlQuery = "INSERT INTO SCHOLARSHIP_TRANSACTION(scholarship_id, usn, time, amount) VALUES ('" + scholarshipID + "', '" + USNBox.Text.ToUpper() + "', timestamp '" + time + "', " + amount + ")";
+        
+                if (sql_operator.Insert(sqlQuery))
+                {
+                    MessageBox.Show("Inserted Entry", "Successful");
+                    Clear_All_Entries();
+                }
+                else
+                {
+                    MessageBox.Show("USN does not exist", "Failed");
+                    MessageBox.Show(sqlQuery);
+                }
+            }
+        }
+
+        private void LabelUSN_Click(object sender, EventArgs e)
+        {
+            USNBox.Select();
+        }
+
+        private void LabelSearch_Click(object sender, EventArgs e)
+        {
+            SearchBox.Select();
+        }
+
+        private void Back_Click(object sender, EventArgs e)
+        {
+            studentTransactionMenu.Show();
+            Hide();
+            Clear_All_Entries();
+        }
+
+        private void ScholarshipTransaction_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+            studentTransactionMenu.Show();
+            Hide();
+            Clear_All_Entries();
+        }
+    }
+}
